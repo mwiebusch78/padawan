037edc23b60c9f28053afb4c5a03c50e6841edbc
Martin Wiebusch <mwiebusch78@gmail.com>
2022-12-14 18:14:25 +0000

    removes forced stats computation from PersistedDataset

--- padawan/persisted_dataset.py
+++ padawan/persisted_dataset.py
@@ -26,55 +26,14 @@ class PersistedDataset(Dataset):
         assert len(files) == len(upper_bounds)
         return files, index_columns, sizes, lower_bounds, upper_bounds
 
-    def _scan_partition(self, path, index_columns):
-        part = pl.scan_parquet(path)
-        if index_columns:
-            part = part.select(index_columns).collect()
-            nrows = len(part)
-            lb = min(part.rows())
-            ub = max(part.rows())
-        else:
-            nrows = part.select(pl.count()).collect().row(0)[0]
-            lb = ()
-            ub = ()
-        filename = os.path.basename(path)
-
-        return filename, nrows, lb, ub
-
-    def _scan_folder(self, path, index_columns, parallel):
+    def _scan_folder(self, path):
         path_pattern = os.path.join(path, '*.parquet')
         partition_paths = sorted(glob.glob(path_pattern))
         if not partition_paths:
             raise FileNotFoundError(
                 f'No files matching pattern {path_pattern}')
 
-        meta = parallel_map(
-            self._scan_partition,
-            partition_paths,
-            workers=parallel,
-            shared_args={'index_columns': index_columns},
-        )
-        files = [m[0] for m in meta]
-        sizes = [m[1] for m in meta]
-        lower_bounds = [m[2] for m in meta]
-        upper_bounds = [m[3] for m in meta]
-
-        return files, sizes, lower_bounds, upper_bounds
-
-    def _project_stats(self, stats, current_index_columns, index_columns):
-        num_index_columns = len(index_columns)
-        num_current_index_columns = len(current_index_columns)
-
-        new_stats = []
-        for obj in stats:
-            new_stats.append(
-                PartitionStats(
-                    nrows=obj.nrows,
-                    lb=obj.lb[:num_index_columns],
-                    ub=obj.ub[:num_index_columns],
-                )
-            )
-        return new_stats
+        return [os.path.basename(f) for f in partition_paths]
 
     def __init__(self, path, index_columns=None, parallel=False):
         self._path = path
@@ -83,13 +42,11 @@ class PersistedDataset(Dataset):
             files, current_index_columns, sizes, lower_bounds, upper_bounds \
                 = self._load_metadata(path)
         except FileNotFoundError:
-            if index_columns is None:
-                raise StatsNotFoundError(
-                    f'No stats found under {path}. '
-                    'Specify index_columns to trigger a scan.')
-            files, sizes, lower_bounds, upper_bounds \
-                = self._scan_folder(path, index_columns, parallel)
-            current_index_columns = tuple(index_columns)
+            files = self._scan_folder(path)
+            sizes = None
+            lower_bounds = None
+            upper_bounds = None
+            current_index_columns = tuple()
 
         if index_columns is not None:
             index_columns = tuple(index_columns)
@@ -98,8 +55,9 @@ class PersistedDataset(Dataset):
             if not (num_index_columns <= num_current_index_columns
                     and index_columns == 
                     current_index_columns[:num_index_columns]):
-                files, sizes, lower_bounds, upper_bounds \
-                    = self._scan_folder(path, index_columns, parallel)
+                sizes = None
+                lower_bounds = None
+                upper_bounds = None
             else:
                 lower_bounds = [b[:num_index_columns] for b in lower_bounds]
                 upper_bounds = [b[:num_index_columns] for b in upper_bounds]
